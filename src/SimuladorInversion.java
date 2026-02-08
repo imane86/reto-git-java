dfe492926b1c9073160a9f0861580e3b87fb4123
Imane <imane.uned@gmail.com>
2026-02-08 16:20:43 +0100

    Mejora mensaje de salida

--- src/SimuladorInversion.java
+++ src/SimuladorInversion.java
@@ -30,7 +30,7 @@ public class SimuladorInversion {
         } else if (gananciaPerdida < 0) {
             System.out.println("Pérdida sufrida: " + gananciaPerdida);
         } else {
-            System.out.println("No hay ganancia ni pérdida.");
+            System.out.println("No hay ganancia ni pérdida, sin cambios.");
         }
         
         sc.close();
